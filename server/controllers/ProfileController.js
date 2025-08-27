fe9a278a502536bc76148400ce6915b30361dbb6
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-27 22:41:53 +0700

    Profile functionality for Admin only

--- server/controllers/ProfileController.js
+++ server/controllers/ProfileController.js
@@ -6,20 +6,131 @@ class ProfileController {
   //   res.json({ message: 'Add a single Profile!!!' })
   // }
 
+  // For Staff/Admin
+  static async getMyProfile(req, res) { }
+  static async updateMyProfile(req, res) { }
+  static async updateMyEmail(req, res) { }
+  static async updateMyPassword(req, res) { }
+
+  // For Admin only
   static async getAllProfiles(req, res) {
-    res.json({ message: 'Get all Profiles!!!' })
+    try {
+      // pagination
+      const limit = Number(req.query.limit) || 10
+      const offset = Number(req.query.offset) || 0
+
+      let { count, rows } = await Profile.findAndCountAll({
+        include: [{
+          model: User,
+          attributes: { exclude: ['password'] } // remove attr password
+        }],
+        limit,
+        offset
+      })
+
+      return res.status(200).json({
+        success: true,
+        data: { total: count, users: rows },
+        message: "Profile fetch success"
+      })
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: err.message
+      })
+    }
   }
 
   static async getOneProfile(req, res) {
-    res.json({ message: 'Get a single Profile!!!' })
+    try {
+      const id = req.params.id
+
+      let result = await Profile.findOne({
+        where: { id },
+        include: [User]
+      })
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          data: null,
+          message: 'Profile ID not found.'
+        })
+      }
+
+      return res.status(200).json({
+        success: true,
+        data: result,
+        message: "Profile fetch success"
+      })
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: err.message
+      })
+    }
   }
 
   static async updateProfile(req, res) {
-    res.json({ message: 'Update a single Profile!!!' })
+    try {
+      const id = req.params.id
+      const { username, imageUrl, fullname, dateOfBirth } = req.body
 
+      const updatedUser = {
+        username, imageUrl, fullname, dateOfBirth
+      }
+
+      let [result] = await Profile.update(updatedUser, { where: { id } })
+      if (result === 0) {
+        // if ID not found when update
+        return res.status(404).json({
+          success: false,
+          data: null,
+          message: `Profile ${id} not found!`
+        })
+      }
+
+      return res.status(200).json({
+        success: true,
+        data: { username, imageUrl, fullname, dateOfBirth },
+        message: `Profile ${id} updated!`
+      })
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: err.message
+      })
+    }
   }
+
   static async deleteProfile(req, res) {
-    res.json({ message: 'Delete a single Profile!!!' })
+    try {
+      const id = req.params.id
+      let result = await Profile.destroy({ where: { id } })
+
+      if (result) {
+        return res.status(200).json({
+          success: true,
+          data: { id },
+          message: `Profile ${id} has been deleted!`
+        })
+      } else {
+        return res.status(404).json({
+          success: false,
+          data: null,
+          message: `Profile ${id} not found!`
+        })
+      }
+
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        data: null,
+        message: err.message
+      })
+    }
   }
 
   static async searchProfile(req, res) {
